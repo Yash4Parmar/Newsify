107b34af46d27a19bb58654a78153135bb824a5e
Yash Parmar <yash4parmar12@gmail.com>
2022-11-14 16:44:02 +0530

    -make news card littele bit dark and changes color of button

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,18 +2,19 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
     render() {
-        const { title, description, urlOfImg, newsUrl } = this.props;
+        const { title, description, urlOfImg, newsUrl, date } = this.props;
         return (
             <div>
-                <div className="card">
+                <div className="card text-bg-light mb-3" >
                     <img src={urlOfImg ? urlOfImg : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSj6FHyL14r8qwbb9VwDr7fUiXkdicySujolA&usqp=CAU"} className="card-img-top" alt="perticuler news pic" />
                     <div className="card-body">
                         <h5 className="card-title">{title}...</h5>
+                        <p className="card-text"><small className="text-muted">{new Date(date).toUTCString()}</small></p>
                         <p className="card-text">{description}</p>
-                        <a to={newsUrl} target="_blank" className="btn btn-primary">Read More</a>
+                        <a href={newsUrl} target="_blank" className="btn btn-dark">Read More</a>
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
